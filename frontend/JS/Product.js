c8873b89f7e2595a2f990663fbbac5089a9bf0b4
Jbens140 <75929738+Jbens140@users.noreply.github.com>
2021-05-14 01:52:17 +0200

    JS page produit + parametrage des href JS

---
+++ frontend/JS/Product.js
@@ -0,0 +1,58 @@
+// console.log(this)
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+
+    if (request.readyState === 4) {
+        var Peluche = JSON.parse(request.responseText);
+        // console.log('****************', Peluche);
+
+        var template = `<div class="FlexProduct">\
+        <img class="ContainerProduct" src="${Peluche.imageUrl}" alt="${Peluche.name}">\
+        <div class="ProductDescription">\
+           <div class="BearNameProduct">${Peluche.name}</div>\
+           <div class="BearPriceProduct">${Peluche.price} €</div>\
+           <select name="Choix de la couleur" id="colorChoice">Please select...</select>\
+           <button class="AddToCart">Ajouter au panier</button>\
+        </div>`
+
+        document.getElementById("productByID").innerHTML = template
+
+
+
+        // ******** liste de select pour les couleurs ********
+        let colorOptions = `<option value="Choisissez votre couleur...">Choisissez votre couleur...</option>`
+        for (let i = 0; i < Peluche.colors.length; i++) {
+            colorOptions += `<option value="${Peluche.colors[i]}">${Peluche.colors[i]}</option>`
+        }
+        document.getElementById("colorChoice").innerHTML = colorOptions
+        // *********
+    }
+};
+
+
+// ************************************* reformate le parametre ID de l'url ***************************************
+let getProductID = function (param) {
+    // console.log(param)
+    let parameters = param.split("?")
+    parameters.shift()
+    // console.log(parameters)
+    let id = parameters[0].substr(3, parameters[0].length)
+    // console.log('*********', id)
+    return id
+}
+//*****************************************************************************************************************
+
+
+
+
+
+// ************************************* fetch peluche par ID *****************************************************
+let productID = getProductID(this.location.search)
+request.open('GET', `http://localhost:3000/api/teddies/${productID}`);
+request.send();
+
+
+
+console.log('**********' ,localStorage)
+console.log('*********************', JSON.parse(localStorage.panier))
+// localStorage.clear()
--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -1,17 +1,18 @@
-console.log('coucou');
 
 
 
 
 var request = new XMLHttpRequest();
 request.onreadystatechange = function () {
+
+    // console.log('*************', request)
     if (request.readyState === 4) {
         var tableauPeluches = JSON.parse(request.responseText);
-        console.log(tableauPeluches);
+        // console.log(tableauPeluches);
         for (var peluches in tableauPeluches){
-            console.log(tableauPeluches[peluches])
+            // console.log(tableauPeluches[peluches])
             var template = ' <div class="container">\
-<a href="">\
+<a href="./ProductPage.html?id='+ tableauPeluches[peluches]._id +'" >\
     <img class="BearImg" src="'+ tableauPeluches[peluches].imageUrl +'" alt="Teddy_1">\
     <div class="BearTitle">'+ tableauPeluches[peluches].name +'</div>\
     <div class="BearPrice">'+ tableauPeluches[peluches].price +'</div>\
@@ -25,6 +26,17 @@ request.onreadystatechange = function () {
 
         }
     }
+    
+
+
 };
 request.open('GET', 'http://localhost:3000/api/teddies');
 request.send();
+
+let array = [{'id':'5be9c8541c9d440000665243', }]
+array = JSON.stringify(array)
+localStorage.setItem('panier', array);
+// localStorage.setItem( 'Tom','monChat');
+
+
+
